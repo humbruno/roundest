3e3b29712dfc3c9e4e888333935ab5de177a1389
Bruno Santos <>
2022-11-28 16:43:35 +0000

    feat: render pokemon image and name

--- src/components/ComparisonItem/index.tsx
+++ src/components/ComparisonItem/index.tsx
@@ -1,12 +1,22 @@
+import Image from 'next/image';
 import React from 'react';
 import styles from './styles.module.scss';
 
 interface ComparisonItemProps {
-  content: number;
+  content: any;
 }
 
 const ComparisonItem = ({ content }: ComparisonItemProps) => {
-  return <div className={styles.placeholder}>{content}</div>;
+  return (
+    <div className={styles.placeholder}>
+      <img
+        className={styles.image}
+        src={content.sprites.front_default}
+        alt=""
+      />
+      <h2>{content.name}</h2>
+    </div>
+  );
 };
 
 export default ComparisonItem;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,33 @@
 import React, { useEffect, useState } from 'react';
-import Comparison from 'components/Comparison';
 import styles from 'styles/Home.module.scss';
 import { getOptionsForVote } from 'utils/getRandomPokemon';
 import type { NextPage } from 'next';
+import { trpc } from 'utils/trpc';
+import ComparisonItem from 'components/ComparisonItem';
 
 const Home: NextPage = () => {
-  const [comparisonItems, setComparisonItems] = useState<number[]>([]);
+  const [ids, updateIds] = useState(() => getOptionsForVote());
 
-  useEffect(() => {
-    const pokemonsToCompare = getOptionsForVote();
-    setComparisonItems(pokemonsToCompare);
-  }, []);
+  const [firstId, secondId] = ids;
+
+  const first = trpc['get-pokemon-by-id'].useQuery({ id: firstId }).data
+    ?.pokemon;
+  const second = trpc['get-pokemon-by-id'].useQuery({ id: secondId }).data
+    ?.pokemon;
+
+  if (!first || !second) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className={styles.title}>
       <h1>Which Pokemon is rounder?</h1>
-      <Comparison itemsToCompare={comparisonItems} />
+      <div className={styles.container}>
+        <ComparisonItem content={first} />
+        <strong>or</strong>
+        <ComparisonItem content={second} />
+      </div>
     </div>
   );
 };
-
 export default Home;
